2ab2b4f3fc01c7aaeb5990a87edaa3fa68fee806
Jonatan <johntechs.dev@gmail.com>
2021-12-23 20:23:31 +0000

    add feature - request unix and utc timestamp in /api/:date

--- server.js
+++ server.js
@@ -8,25 +8,51 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(_, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(_, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+//Debug request
+app.use((req, _, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
+app.use("/api/:date(\\d+)", (req, _, next) => {
+  req.date = parseInt(req.params.date) * 1000;
+  next();
+});
+
+app.use("/api/:date(\\d{4}-\\d{2}-\\d{2})", (req, _, next) => {
+  req.date = req.params.date;
+  next();
+});
+
+app.use("/api/:date", (req, res) => {
+  if (req.date) {
+    const date = new Date(req.date);
+    return res.json(
+      {
+        unix: date.getTime(),
+        utc: new Date(date).toUTCString(),
+        date: req.params.date
+      }
+    );
+  }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
